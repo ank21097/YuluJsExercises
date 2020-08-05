2059389eabbdc81d68ca88183368794e8d59473a
ank21097 <44895494+ank21097@users.noreply.github.com>
2020-08-05 20:51:51 +0530

    Add files via upload

---
+++ capitalize/index.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+
+    return str.join(" ");
+}
+
+module.exports = capitalize;
